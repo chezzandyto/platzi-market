1e2b6d7a93e22f1733ecdd60a06c5ae131f447d9
chezzandyto <andres_toapanta@hotmail.com>
2022-06-26 19:59:42 -0500

    creado el modelo ORM sin relaciones entre tablas

---
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -0,0 +1,85 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity //esta anotacion sirve para decir que esta clase mapea una tabla
+@Table(name = "productos") // esta anotacion le dice que nombre tiene la tabla en especifico
+public class Producto {
+
+    @Id //anotacion para decir que tiene llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Le dice como añadir la secuencia de ID, de forma automatica
+    @Column(name = "id_producto") // anotacion para decir que tiene nombre diferente en la DB
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private  Integer cantidadStock;
+
+    private Boolean estado;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}

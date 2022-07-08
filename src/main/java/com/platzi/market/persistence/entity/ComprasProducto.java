418a773543c2d85233896c8e96a8c0c4c8dacc69
chezzandyto <andres_toapanta@hotmail.com>
2022-07-07 21:16:51 -0500

    creado el controlador y servicio para las compras PURCHASES

--- src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -16,6 +16,7 @@ public class ComprasProducto {
     private Boolean estado;
 
     @ManyToOne
+    @MapsId("idCompra")
     @JoinColumn(name = "id_compra", insertable = false, updatable = false)
     private Compra compra;
 
@@ -25,6 +26,22 @@ public class ComprasProducto {
 
     public ComprasProductoPK getId() {
         return id;
+    }
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Compra compra) {
+        this.compra = compra;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
     }
 
     public void setId(ComprasProductoPK id) {

357ac810b5cf0ede64189994336d09d372b950bb
chezzandyto <andres_toapanta@hotmail.com>
2022-07-18 19:48:29 -0500

    implementada la seguridad para la API con usuario y contrasena

--- src/main/java/com/platzi/market/web/config/SwaggerConfig.java
+++ src/main/java/com/platzi/market/web/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.platzi.market.web.controller.config;
+package com.platzi.market.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
